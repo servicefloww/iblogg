636223f05c973169d593a7a8a0f6bc4d7825997c
servicefloww <servicefloww@gmail.com>
2023-03-16 22:58:11 +0530

    This is sample blogging website

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function navfunc(){
+    navBar = document.getElementById("nav__bar")
+    navBar.classList.toggle("active")
+    hide = document.getElementById("hide")
+    hide.classList.toggle("show")
+}
+function load(){
+    load = document.getElementById("load")
+    load.classList.toggle("active")
+}
